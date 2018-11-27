d1fa1ad60d2313ab317d269861b8eef63db8679a
Caio Santos <caio.f.santos@icloud.com>
2018-11-27 01:11:39 -0200

    Initial styles aplied for all screens. Style for large screens completed. Planning button for mobile devices with some javascript.
    A folder regarding those scripts was created within assets folder.

--- gulp/tasks/styles.js
+++ gulp/tasks/styles.js
@@ -5,10 +5,11 @@ var cssVars = require('postcss-simple-vars');
 var nested = require('postcss-nested');
 var cssImport = require('postcss-import');
 var mixins = require('postcss-mixins');
+var hexrgba = require('postcss-hexrgba');
 
 gulp.task('styles', function(){
 	return gulp.src('./app/assets/styles/style.css')
-        .pipe(postcss([cssImport, mixins, cssVars, nested, autoprefixer]))
+        .pipe(postcss([cssImport, mixins, cssVars, nested, hexrgba, autoprefixer]))
         // This line prevents watch to stop running after an error occurs on css compile
         .on('error', function(errorInfo){
             console.log(errorInfo.toString());
